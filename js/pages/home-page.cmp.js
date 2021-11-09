d25b523034ecab7987564903e46829bdf2e58e92
Evgey <evgeny12712@gmail.com>
2021-11-09 14:50:47 +0200

    logo fix v2

--- js/pages/home-page.cmp.js
+++ js/pages/home-page.cmp.js
@@ -1,7 +1,7 @@
 export default {
     template: `
         <section class="home-page">
-            <img src="../../img/book-shop-home.png"/>
+            <img src="../../img/book-logo.png"/>
          <p>Online Book store is an online web application where the customer can purchase books online. Through a web browser the customers can search for a book by its title or author, later can add to the shopping cart and finally purchase using credit card transaction.</p>
         </section>
     `
